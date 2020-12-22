78cba24e180676665027c2294c9c8d621d014eaa
Bboy <bornscience1@gmail.com>
2020-12-21 20:32:20 -0800

    fix script

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -53,6 +53,112 @@ $(document).ready(function() {
     
 //search section
 
+function delaySearch(s){
+
+	setTimeout(function(){
+
+		var input = document.querySelector('.search-modal .search-bar .centered .search-input');
+		if(input.value.trim() == s && input.value.trim().length > 0){
+
+			if(input.getAttribute('data-active') != s){
+				input.setAttribute('data-active', s);
+
+				ga('send', 'pageview', '/search');
+
+				$.get('https://www.instagram.com/web/search/topsearch/?context=blended&query='+s+'&include_reel=false&count=50', function(r){
+
+					document.querySelector('.search-modal').classList.remove('loading');
+
+					if(r.status == 'ok'){
+
+						if(r.users.length > 0){
+
+							document.querySelector('.search-modal').classList.add('has-results');
+
+						} else {
+
+							document.querySelector('.search-modal').classList.add('no-results');
+						}
+
+						var list = document.querySelectorAll('.search-modal .search-results .centered .results-search-list .account');
+
+						for(var i = 0; i < list.length; i++){
+
+							var li = list[i];
+
+							var item = r.users[i].user;
+
+							if(item === undefined){
+
+								li.style.display = 'none';
+								li.classList.remove('verified');
+								li.querySelector('a').href = 'stories.html';
+								//li.querySelector('a').on('click', null);
+
+								var div = li.querySelector('a .avatar');
+								div.style.backgroundImage = 'none';
+
+								var span1 = li.querySelector('a .username');
+								span1.innerHTML = 'username';
+
+								var span2 = li.querySelector('a .name');
+								span2.innerHTML = 'subname';
+
+								continue;
+							}
+
+							li.style.display = 'block';
+
+							if(li.getAttribute('data-type') == 'stories'){
+
+								li.querySelector('a').href = '/stories/'+item.username;
+
+							} else {
+
+								li.querySelector('a').href = '/profile/'+item.username;
+							}
+
+							var div = li.querySelector('a .avatar');
+							div.style.backgroundImage = 'url(\''+item.profile_pic_url+'\')';
+
+							var span1 = li.querySelector('a .username');
+							span1.innerHTML = item.username;
+							if(item.is_verified != null && item.is_verified === true){
+								var el = document.createElement('i');
+								el.classList.add('verified');
+								span1.appendChild(el);
+								li.classList.add('verified');
+							}
+
+							var span2 = li.querySelector('a .name');
+							span2.innerHTML = '';
+							if(item.full_name != null && item.full_name.length > 0){
+								span2.innerHTML = item.full_name;
+							}
+						}
+
+						ga('send', 'pageview', '/search/results');
+
+						return;
+					}
+
+					document.querySelector('.search-modal').classList.remove('loading');
+					document.querySelector('.search-modal').classList.add('error');
+
+					ga('send', 'pageview', '/search/error-2');
+
+
+				}).fail(function(xhr, status, error) {
+
+					document.querySelector('.search-modal').classList.remove('loading');
+					document.querySelector('.search-modal').classList.add('error');
+
+					ga('send', 'pageview', '/search/error-1');
+				});
+			}
+		}
+	}, 3000);
+}
     function refreshSearch(){
         document.querySelector('.search-modal').classList.remove('no-results');
         document.querySelector('.search-modal').classList.remove('has-results');
@@ -115,7 +221,7 @@ $(document).ready(function() {
         e.preventDefault();
         
         refreshSearch();
-    };function refreshSearch(){
+    };function refreshSearch(){.
 	document.querySelector('.search-modal').classList.remove('no-results');
 	document.querySelector('.search-modal').classList.remove('has-results');
 	document.querySelector('.search-modal').classList.remove('error');
